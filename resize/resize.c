7dd72e9d51beb25f71ff961c7646dd6980dfdb5c
Akshay kamboj <akskamboj1998@gmail.com>
2017-05-08 17:30:17 +0000

    resize basic code

--- resize/resize.c
+++ resize/resize.c
@@ -5,12 +5,95 @@
 
 int main(int argc, char *argv[])
 {
-    // ensure proper usage
     if (argc != 3)
     {
         fprintf(stderr, "Usage: ./copy infile outfile\n");
         return 1;
     }
-return 0;
+
+   
+    char *infile = argv[1];
+    char *outfile = argv[2];
+
     
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", infile);
+        return 2;
+    }
+
+    
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        fprintf(stderr, "Could not create %s.\n", outfile);
+        return 3;
+    }
+
+    
+    BITMAPFILEHEADER bf;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+
+    
+    BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+
+    
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || 
+        bi.biBitCount != 24 || bi.biCompression != 0)
+    {
+        fclose(outptr);
+        fclose(inptr);
+        fprintf(stderr, "Unsupported file format.\n");
+        return 4;
+    }
+    
+
+    int originalWidth = bi.biWidth;
+    int originalHeight = bi.biHeight;
+   
+   
+    bi.biWidth *= scale;
+    bi.biHeight *= scale;
+       
+   
+    int originalPadding =  (4 - (originalWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    
+    bi.biSizeImage = abs(bi.biHeight) * ((bi.biWidth * sizeof (RGBTRIPLE)) + padding);
+    
+    
+    bf.bfSize = bi.biSizeImage + sizeof (BITMAPFILEHEADER) + sizeof (BITMAPINFOHEADER);
+    
+    
+    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+
+   
+    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+
+   
+    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    
+   
+    RGBTRIPLE *buffer = malloc(sizeof(RGBTRIPLE) * (bi.biWidth));
+
+    
+   
+            
+       
+      
+
+   
+    free(buffer);
+
+    
+    fclose(inptr);
+
+   
+    fclose(outptr);
+
+  
+    return 0;
 }
