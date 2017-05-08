2bf5088f12d86ddbe5be1f03ed39be734db3d3fe
Akshay kamboj <akskamboj1998@gmail.com>
2017-05-08 17:39:33 +0000

    programme runs

--- resize/resize.c
+++ resize/resize.c
@@ -1,3 +1,5 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,17 +7,18 @@
 
 int main(int argc, char *argv[])
 {
+    // ensure proper usage
     if (argc != 3)
     {
         fprintf(stderr, "Usage: ./copy infile outfile\n");
         return 1;
     }
 
-   
+    // remember filenames
     char *infile = argv[1];
     char *outfile = argv[2];
 
-    
+    // open input file 
     FILE *inptr = fopen(infile, "r");
     if (inptr == NULL)
     {
@@ -23,7 +26,7 @@ int main(int argc, char *argv[])
         return 2;
     }
 
-    
+    // open output file
     FILE *outptr = fopen(outfile, "w");
     if (outptr == NULL)
     {
@@ -32,15 +35,15 @@ int main(int argc, char *argv[])
         return 3;
     }
 
-    
+    // read infile's BITMAPFILEHEADER
     BITMAPFILEHEADER bf;
     fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
 
-    
+    // read infile's BITMAPINFOHEADER
     BITMAPINFOHEADER bi;
     fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
 
-    
+    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
     if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || 
         bi.biBitCount != 24 || bi.biCompression != 0)
     {
@@ -50,46 +53,47 @@ int main(int argc, char *argv[])
         return 4;
     }
     
-
+    // original width and height
     int originalWidth = bi.biWidth;
     int originalHeight = bi.biHeight;
    
-   
+    //  width and height
     bi.biWidth *= scale;
     bi.biHeight *= scale;
        
-   
+    //  original padding 
     int originalPadding =  (4 - (originalWidth * sizeof(RGBTRIPLE)) % 4) % 4;
     
+    // update image size
     bi.biSizeImage = abs(bi.biHeight) * ((bi.biWidth * sizeof (RGBTRIPLE)) + padding);
     
-    
+    // update file size
     bf.bfSize = bi.biSizeImage + sizeof (BITMAPFILEHEADER) + sizeof (BITMAPINFOHEADER);
     
-    
+    // write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
-   
+    // write outfile's BITMAPINFOHEADER
     fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
 
-   
+    // determine padding for scanlines
     int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
 
     
-   
+    // allocate storage for holding rgb 
     RGBTRIPLE *buffer = malloc(sizeof(RGBTRIPLE) * (bi.biWidth));
 
-    
+    // iterate over files rgb
     for (int i = 0, biHeight = abs(originalHeight); i < biHeight; i++)
     {
         int tracker = 0;
-       
+        // iterate over pixels in scanline
         for (int j = 0; j < originalWidth; j++)
         {
-           
+            // temporary storage for rgb
             RGBTRIPLE triple;
             
-           
+            // read RGB triple from infile
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
             
            
@@ -108,19 +112,19 @@ int main(int argc, char *argv[])
            {
                 fwrite((buffer), sizeof(RGBTRIPLE), bi.biWidth, outptr);
 
-               
+                
                 for (int k = 0; k < padding; k++)
                     fputc(0x00, outptr);
            }        
     }
 
-   
+    
     free(buffer);
 
-    
+   
     fclose(inptr);
 
-   
+    
     fclose(outptr);
 
   
