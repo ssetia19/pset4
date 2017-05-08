79412d5d8b220dd795c1250a88fcc61be6044936
Akshay kamboj <akskamboj1998@gmail.com>
2017-05-08 16:51:42 +0000

    whodunit code completed

--- whodunit/whodunit.c
+++ whodunit/whodunit.c
@@ -1,15 +1,98 @@
-include <stdio.h>
-#include <stdlib.h>
+// remember filenames
+    char *infile = argv[1];
+    char *outfile = argv[2];
 
-#include "bmp.h"
-
-int main(int argc, char *argv[])
-{
-    // ensure proper usage
-    if (argc != 3)
+   
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
     {
-        fprintf(stderr, "Usage: ./copy infile outfile\n");
-        return 1;
+        fprintf(stderr, "Could not open %s.\n", infile);
+        return 2;
     }
+
+  
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        fprintf(stderr, "Could not create %s.\n", outfile);
+        return 3;
+    }
+
+   
+    BITMAPFILEHEADER bf;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+
+   
+    BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+
+   
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || 
+        bi.biBitCount != 24 || bi.biCompression != 0)
+    {
+        fclose(outptr);
+        fclose(inptr);
+        fprintf(stderr, "Unsupported file format.\n");
+        return 4;
+    }
+
+   
+    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+
+    
+    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+
+   
+    int padding =  (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    
+    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
+    {
+        
+        for (int j = 0; j < bi.biWidth; j++)
+        {
+           
+            RGBTRIPLE triple;
+
+           
+            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+            
+            if(triple.rgbtBlue == 0x00 && triple.rgbtGreen == 0x00)
+                triple.rgbtRed = 0x00;
+            
+            else if(triple.rgbtBlue == 0x00 && triple.rgbtGreen == 0x00&&triple.rgbtRed == 0x00)
+                {
+                    triple.rgbtBlue = 0xff;triple.rgbtGreen = 0xff;triple.rgbtRed = 0xff;
+                }
+                else if(triple.rgbtBlue == 0xFF && triple.rgbtGreen == 0xFF && triple.rgbtRed == 0xFF){
+                    
+                }
+                
+            else{
+                triple.rgbtBlue = 0x00;triple.rgbtGreen = 0x00;triple.rgbtRed = 0x00;
+            }
+            
+           
+            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+        }
+
+       
+        fseek(inptr, padding, SEEK_CUR);
+
+       
+        for (int k = 0; k < padding; k++)
+        {
+            fputc(0x00, outptr);
+        }
+    }
+
+   
+    fclose(inptr);
+
+   
+    fclose(outptr);
+
+    
     return 0;
-    }
+}
