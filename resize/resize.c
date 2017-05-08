930732d6ecc7b5d295f6c286d81c63213cc02297
Akshay kamboj <akskamboj1998@gmail.com>
2017-05-08 17:11:05 +0000

    resize  initialisation

---
+++ resize/resize.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bmp.h"
+
+int main(int argc, char *argv[])
+{
+    // ensure proper usage
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: ./copy infile outfile\n");
+        return 1;
+    }
+return 0;
+    
+}
